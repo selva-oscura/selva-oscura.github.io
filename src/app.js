5c23b117c0ed40d54fa0c58cb714f8dc2d6dd138
Carol St.Louis <stlouis_c@yahoo.com>
2016-09-20 21:27:24 -0700

    updated .createClass passing title and color props with default color and title text values

--- src/app.js
+++ src/app.js
@@ -7,8 +7,8 @@ var h1 = React.DOM.h1;
 var MyTitle = React.createClass({
 	render () {
 		return (
-			div(null, 
-				h1(null, "Hallo Welt")
+			div({style: {color:"red"} }, 
+				h1({style:{color: this.props.color}} || null, this.props.title || "Hallo Vorgabewert")
 			)
 		)
 	}
@@ -16,7 +16,9 @@ var MyTitle = React.createClass({
 
 var TitleComponent = (
 	div(null,
-		React.createElement(MyTitle, null)
+		React.createElement(MyTitle, null),
+		React.createElement(MyTitle, {color:"#4078C0"}),
+		React.createElement(MyTitle, {color:"#006400", title:"Hallo Welt"})
 	)
 );
 
