fc4330e81a7bc76c725eb8fb2e59c181496c510f
Carol St.Louis <stlouis_c@yahoo.com>
2016-09-20 21:28:27 -0700

    updated public/bundle.js to reflect changes to src files

--- public/bundle.js
+++ public/bundle.js
@@ -53,8 +53,8 @@
 	var MyTitle = React.createClass({
 		render () {
 			return (
-				div(null, 
-					h1(null, "Hallo Welt")
+				div({style: {color:"red"} }, 
+					h1({style:{color: this.props.color}} || null, this.props.title || "Hallo Vorgabewert")
 				)
 			)
 		}
@@ -62,7 +62,9 @@
 
 	var TitleComponent = (
 		div(null,
-			React.createElement(MyTitle, null)
+			React.createElement(MyTitle, null),
+			React.createElement(MyTitle, {color:"#4078C0"}),
+			React.createElement(MyTitle, {color:"#006400", title:"Hallo Welt"})
 		)
 	);
 
