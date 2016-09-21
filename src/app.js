5068bd03de6dc6c70703168aa5f0a09ace4a3ca3
Carol St.Louis <stlouis_c@yahoo.com>
2016-09-20 21:04:50 -0700

    changed name for component

--- src/app.js
+++ src/app.js
@@ -4,11 +4,10 @@ var ReactDOM = require('react-dom');
 var div = React.DOM.div;
 var h1 = React.DOM.h1;
 
-var MyFirstComponent = (
- div(null,
-     h1(null, "Hello React!!")
- )
+var TitleComponent = (
+	div(null,
+		h1(null, "Hello React!!")
+	)
 );
 
-ReactDOM.render(MyFirstComponent, document.getElementById('app')
-    );
+ReactDOM.render(TitleComponent, document.getElementById('app'));
