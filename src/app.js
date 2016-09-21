b57a1b0c6f2363a1a0c97cd278463899fd2c9f50
Carol St.Louis <stlouis_c@yahoo.com>
2016-09-20 21:12:24 -0700

    changed component to a ReactDOM.render calling the TitleComponent, which then creates and Element using the MyTitle class

--- src/app.js
+++ src/app.js
@@ -4,9 +4,19 @@ var ReactDOM = require('react-dom');
 var div = React.DOM.div;
 var h1 = React.DOM.h1;
 
+var MyTitle = React.createClass({
+	render () {
+		return (
+			div(null, 
+				h1(null, "Hallo Welt")
+			)
+		)
+	}
+});
+
 var TitleComponent = (
 	div(null,
-		h1(null, "Hello React!!")
+		React.createElement(MyTitle, null)
 	)
 );
 
