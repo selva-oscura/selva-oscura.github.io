0140cf6149ca512c8d419b15ad35f1141e00173f
Carol St.Louis <stlouis_c@yahoo.com>
2016-09-20 21:59:41 -0700

    modularized MyTitle as src/MyTitle.js and required it in src/app.js

--- src/app.js
+++ src/app.js
@@ -1,24 +1,21 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var MyTitle = require('./MyTitle.js');
+// import { MyTitle } from './MyTitle.js';
 
 var div = React.DOM.div;
 var h1 = React.DOM.h1;
 
-var MyTitle = React.createClass({
-	render () {
-		return (
-			div({style: {color:"red"} }, 
-				h1({style:{color: this.props.color}} || null, this.props.title || "Hallo Vorgabewert")
-			)
-		)
-	}
-});
+var MyTitleFactory = React.createFactory(MyTitle);
 
 var TitleComponent = (
 	div(null,
 		React.createElement(MyTitle, null),
+		React.createElement(MyTitle, {title:"Hello World"}),
 		React.createElement(MyTitle, {color:"#4078C0"}),
-		React.createElement(MyTitle, {color:"#006400", title:"Hallo Welt"})
+		React.createElement(MyTitle, {color:"#006400", title:"Hello World with different colour"}),
+		MyTitleFactory(null),
+		MyTitleFactory({color:"#4078C0", title:"Now with Factory!"})
 	)
 );
 
