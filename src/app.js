ed8e701ea5818d556992391bc03688801d1b515c
Carol St.Louis <stlouis_c@yahoo.com>
2016-09-25 17:47:08 -0700

    roughed out basic layout of profile page (navigation bar, links to navigate to the four sections of the page, the four sections)

--- src/app.js
+++ src/app.js
@@ -1,22 +1,62 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-var MyTitle = require('./MyTitle.js');
+// var MyTitle = require('./MyTitle.js');
 // import { MyTitle } from './MyTitle.js';
+var Section = require('./Section.js');
 
+var section = React.DOM.section;
+var nav = React.DOM.nav;
 var div = React.DOM.div;
 var h1 = React.DOM.h1;
+var h2 = React.DOM.h2;
+var h3 = React.DOM.h2;
+var p = React.DOM.p;
+var a = React.DOM.a;
+var em = React.DOM.em;
+var span = React.DOM.span;
+var ul = React.DOM.ul;
+var li = React.DOM.li;
+var img = React.DOM.img;
 
-var MyTitleFactory = React.createFactory(MyTitle);
-
-var TitleComponent = (
+// var NavFactory = React.createFactory(Nav);
+var SectionFactory = React.createFactory(Section);
+var PageComponent = (
 	div(null,
-		React.createElement(MyTitle, null),
-		React.createElement(MyTitle, {title:"Hello World"}),
-		React.createElement(MyTitle, {color:"#4078C0"}),
-		React.createElement(MyTitle, {color:"#006400", title:"Hello World with different colour"}),
-		MyTitleFactory(null),
-		MyTitleFactory({color:"#4078C0", title:"Now with Factory!"})
+		nav(null, 
+			div({className:'container'},
+				ul(null,
+					li(null,
+						a({
+							className:'navscroll active',
+							id: 'home', 
+						}, 'HOME')
+					),
+					li(null,
+						a({
+							className:'navscroll',
+							id: 'about', 
+						}, 'ABOUT')
+					),
+					li(null,
+						a({
+							className:'navscroll',
+							id: 'portfolio', 
+						}, 'PORTFOLIO')
+					),
+					li(null,
+						a({
+							className:'navscroll',
+							id: 'contact', 
+						}, 'CONTACT')
+					)
+
+				)
+			)			
+		),
+		SectionFactory({className:'home'}),
+		SectionFactory({className:'about'}),
+		SectionFactory({className:'portfolio'}),
+		SectionFactory({className:'contact'})
 	)
 );
-
-ReactDOM.render(TitleComponent, document.getElementById('app'));
+ReactDOM.render(PageComponent, document.getElementById('app'));
