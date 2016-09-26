702e8bdac7db67b1d78a177da72d3865a64e8fb0
Carol St.Louis <stlouis_c@yahoo.com>
2016-09-25 17:50:15 -0700

    updated bundle to reflect changes from basic title react app to rough layout of profile page

--- public/bundle.js
+++ public/bundle.js
@@ -46,26 +46,66 @@
 
 	var React = __webpack_require__(1);
 	var ReactDOM = __webpack_require__(34);
-	var MyTitle = __webpack_require__(172);
+	// var MyTitle = require('./MyTitle.js');
 	// import { MyTitle } from './MyTitle.js';
+	var Section = __webpack_require__(172);
 
+	var section = React.DOM.section;
+	var nav = React.DOM.nav;
 	var div = React.DOM.div;
 	var h1 = React.DOM.h1;
+	var h2 = React.DOM.h2;
+	var h3 = React.DOM.h2;
+	var p = React.DOM.p;
+	var a = React.DOM.a;
+	var em = React.DOM.em;
+	var span = React.DOM.span;
+	var ul = React.DOM.ul;
+	var li = React.DOM.li;
+	var img = React.DOM.img;
 
-	var MyTitleFactory = React.createFactory(MyTitle);
-
-	var TitleComponent = (
+	// var NavFactory = React.createFactory(Nav);
+	var SectionFactory = React.createFactory(Section);
+	var PageComponent = (
 		div(null,
-			React.createElement(MyTitle, null),
-			React.createElement(MyTitle, {title:"Hello World"}),
-			React.createElement(MyTitle, {color:"#4078C0"}),
-			React.createElement(MyTitle, {color:"#006400", title:"Hello World with different colour"}),
-			MyTitleFactory(null),
-			MyTitleFactory({color:"#4078C0", title:"Now with Factory!"})
+			nav(null, 
+				div({className:'container'},
+					ul(null,
+						li(null,
+							a({
+								className:'navscroll active',
+								id: 'home', 
+							}, 'HOME')
+						),
+						li(null,
+							a({
+								className:'navscroll',
+								id: 'about', 
+							}, 'ABOUT')
+						),
+						li(null,
+							a({
+								className:'navscroll',
+								id: 'portfolio', 
+							}, 'PORTFOLIO')
+						),
+						li(null,
+							a({
+								className:'navscroll',
+								id: 'contact', 
+							}, 'CONTACT')
+						)
+
+					)
+				)			
+			),
+			SectionFactory({className:'home'}),
+			SectionFactory({className:'about'}),
+			SectionFactory({className:'portfolio'}),
+			SectionFactory({className:'contact'})
 		)
 	);
-
-	ReactDOM.render(TitleComponent, document.getElementById('app'));
+	ReactDOM.render(PageComponent, document.getElementById('app'));
 
 /***/ },
 /* 1 */
@@ -21438,20 +21478,17 @@
 /***/ function(module, exports, __webpack_require__) {
 
 	var React = __webpack_require__(1);
-	var div = React.DOM.div;
-	var h1 = React.DOM.h1;
+	var section = React.DOM.section;
 
-	var MyTitle = React.createClass({
+	var Section = React.createClass({
 		render () {
 			return (
-				div({style: {color:"red"} }, 
-					h1({style:{color: this.props.color}} || null, this.props.title || "Hello Default Text")
-				)
+				section({className: this.props.className })
 			)
 		}
 	});
 
-	module.exports = MyTitle
+	module.exports = Section
 
 /***/ }
 /******/ ]);
