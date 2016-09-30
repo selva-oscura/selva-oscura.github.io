f181065b8e874c584abca07623d19c82cc441141
Carol St.Louis <stlouis_c@yahoo.com>
2016-09-29 21:44:33 -0700

    added component for displaying skills area, skills with icons/images, and subskills with icons/images

---
+++ src/AboutTechnicalSkills.js
@@ -0,0 +1,81 @@
+var React = require('react');
+var div = React.DOM.div;
+var h3 = React.DOM.h3;
+var p = React.DOM.p;
+var a = React.DOM.a;
+var ul = React.DOM.ul;
+var li = React.DOM.li;
+var em = React.DOM.em;
+var span = React.DOM.span;
+var img = React.DOM.img;
+
+var AboutTechnicalSkills = React.createClass({
+	render(){
+		// console.log('this',this,'this.props', this.props, this.props.children)
+		var target = "topic"+this.props.children
+		// console.log("target", target)
+		// console.log('topic',this.props.topic)
+		return(
+			div(null,
+				h3({key:target}, this.props.topic),
+				ul({key:'ul'+target},
+					this.props.skills.map(function(skill, index){
+						console.log('skill'+target+index);
+						console.log("skill",skill);
+						return li({key: 'skill'+target+index},
+							( skill.icon ?
+								span({key: 'iconspan'+target+index, className: 'onethird'},
+									em({
+										className: skill.icon,
+										style: {color: skill.color}
+									})
+								)
+							:
+								span({key: 'iconspan'+target+index, className: 'onethird'},
+									img({
+										className: skill.className, 
+										src:skill.img, 
+										alt: skill.text
+									})
+								)
+							),
+							span({key: 'textspan'+target+index, className: 'twothird'},
+								skill.text
+							),
+							(skill.subSkills ? 
+								ul({key:'subskillsUl'+target+index},
+									skill.subSkills.map(function(subSkill, index){
+										return li(null,
+											( subSkill.icon ?
+												span({className: 'onethird'},
+													em({
+														className: subSkill.icon,
+														style: {color: subSkill.color}
+													})
+												)
+											:
+												span({className: 'onethird'},
+													img({
+														className: subSkill.className, 
+														src:subSkill.img, 
+														alt: subSkill.text
+													})
+												)							
+											),
+											span({className: 'twothird'},
+												subSkill.text
+											)
+										)
+									})
+								)
+							: null
+							)
+						)
+					})
+				)
+			)
+		)
+	}
+})
+
+module.exports = AboutTechnicalSkills;
