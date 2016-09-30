33055930ed691348f43a8bfa6e9bf84c292255fe
Carol St.Louis <stlouis_c@yahoo.com>
2016-09-29 21:48:54 -0700

    added div with container class to Section Component (not currently using the component, though)

--- src/Section.js
+++ src/Section.js
@@ -1,11 +1,16 @@
 var React = require('react');
+
 var section = React.DOM.section;
+var div = React.DOM.div;
 
 var Section = React.createClass({
 	render () {
 		return (
-			section({className: this.props.className })
+			section({className: this.props.className },
+				div({className: 'container'})
+			)
 		)
+
 	}
 });
 
