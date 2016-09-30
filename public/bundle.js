39894d11a3cf64d584f4a82490aa297e9df77405
Carol St.Louis <stlouis_c@yahoo.com>
2016-09-29 21:45:09 -0700

    updated bundle to reflect changes made to app

--- public/bundle.js
+++ public/bundle.js
@@ -22248,6 +22248,173 @@
 	var em = React.DOM.em;
 	var span = React.DOM.span;
 	var img = React.DOM.img;
+	var AboutTechnicalSkills = __webpack_require__(180);
+	// var TechnicalSkills = require('./TechnicalSkills.js')
+
+	var technicalSkills = [
+		{
+			topic: 'Front-end',
+			loc: 'left',
+			skills:[
+				{
+					icon: 'icon-html5',
+					color: '#E95528',
+					text: 'HTML5'
+				},{
+					icon: 'icon-css3',
+					color: '#2277FF',
+					text: 'CSS3'
+				},{
+					icon: 'icon-bootstrap',
+					color: '#563B7E',
+					text: 'Bootstrap'
+				},{
+					img: './public/img/d3.png',
+					className: 'imgIcon',
+					text: 'D3.js'
+				},{
+					icon: 'icon-jquery',
+					color: '#0868AB',
+					text: 'jQuery'
+				}
+			]
+		},{
+			topic: 'Databases',
+			loc: 'left',
+			skills:[
+				{
+					icon: 'icon-mongodb',
+					color: '#419E39',
+					text: 'MongoDB'
+				},{
+					icon: 'icon-mysql',
+					color: '#425174',
+					text: 'MySQL'
+				}
+			]
+		},{
+			topic: 'PaaS & DBaaS',
+			loc: 'left',
+			skills:[
+				{
+					icon: 'icon-heroku',
+					color: '#6762A6',
+					text: 'Heroku'
+				},{
+					img: './public/img/meteor.png',
+					className: 'imgIcon',
+
+					text: 'Galaxy'
+				},{
+					img: './public/img/mlab.png',
+					className: 'imgIcon',
+					text: 'mLab'
+				}
+			]
+		},{
+			topic: 'Version Control',
+			loc: 'left',
+			skills:[
+				{
+					icon: 'icon-github-circle',
+					color: '#171515',
+					text: 'GitHub'
+				},{
+					img: './public/img/bitbucket.png',
+					className: 'imgIcon',
+					text: 'BitBucket'
+				}
+			]
+		},{
+			topic: 'Languages',
+			loc: 'right',
+			skills:[
+				{
+					icon: 'icon-js',
+					color: '#F1BF26',
+					text: 'JavaScript / ES6'
+				},{
+					icon: 'icon-php',
+					color: '#5A68A5',
+					text: 'PHP'
+				},{
+					img: './public/img/python.png',
+					className: 'imgIcon',
+					text: 'Python'
+				},{
+					icon: 'icon-ruby',
+					color: '#BD1E12',
+					text: 'Ruby'
+				}
+			]
+		},{
+			topic: 'JavaScript Frameworks',
+			loc: 'right',
+			skills:[
+				{
+					img: './public/img/meteor.png',
+					className: 'imgIcon',
+					text: 'Meteor',
+					subSkills: [
+						{
+							icon: 'icon-mongodb',
+							color: '#419E39',
+							text: 'MongoDB'
+						},{
+							icon: 'icon-nodejs',
+							color: '#539E43',
+							text: 'Node.js'
+						},{
+							img: './public/img/blaze.png',
+							className: 'imgIcon',
+							text: 'Blaze / Spacebars'
+						}
+					]
+				},{
+					icon: 'icon-react',
+					text: 'React.js'
+				},{
+					img:  './public/img/mean.png',
+					className: 'imgIcon',
+					text: 'MEAN',
+					subSkills: [
+						{
+							icon: 'icon-mongodb',
+							color: '#419E39',
+							text: 'MongoDB'
+						},{
+							icon: 'icon-express',
+							color: 'black',
+							text: 'Express.js'
+						},{
+							icon: 'icon-angular',
+							color: '#DD1B16',
+							text: 'Angular.js'
+						},{
+							icon: 'icon-nodejs',
+							color: '#539E43',
+							text: 'Node.js'
+						}
+					]
+				}
+			]
+		},{
+			topic: 'Operating Systems',
+			loc: 'right',
+			skills:[
+				{
+					icon: 'icon-ubuntu',
+					color: '#DD4814',
+					text: 'Ubuntu'
+				},{
+					img: './public/img/windows.png',
+					className: 'imgIcon',
+					text: 'Windows'
+				}
+			]
+		}
+	]
+
 
 	var About = React.createClass({
 		render(){
@@ -22255,9 +22422,15 @@
 				section({className:'about'},
 					div({className:'container'},
 						div({className:'half'},
-						'Left'),
+							technicalSkills.map(function(skillsArea, idx){
+								if(skillsArea.loc === "left"){ return React.createElement(AboutTechnicalSkills, skillsArea, idx)}
+							})
+						),
 						div({className:'half'},
-						'Right')
+							technicalSkills.map(function(skillsArea, idx){
+								if(skillsArea.loc === "right"){return React.createElement(AboutTechnicalSkills, skillsArea, idx)}
+							})
+						)
 					)
 				)
 			)
@@ -22266,6 +22439,92 @@
 
 
 	module.exports = About;
+
+/***/ },
+/* 180 */
+/***/ function(module, exports, __webpack_require__) {
+
+	var React = __webpack_require__(1);
+	var div = React.DOM.div;
+	var h3 = React.DOM.h3;
+	var p = React.DOM.p;
+	var a = React.DOM.a;
+	var ul = React.DOM.ul;
+	var li = React.DOM.li;
+	var em = React.DOM.em;
+	var span = React.DOM.span;
+	var img = React.DOM.img;
+
+	var AboutTechnicalSkills = React.createClass({
+		render(){
+			// console.log('this',this,'this.props', this.props, this.props.children)
+			var target = "topic"+this.props.children
+			// console.log("target", target)
+			// console.log('topic',this.props.topic)
+			return(
+				div(null,
+					h3({key:target}, this.props.topic),
+					ul({key:'ul'+target},
+						this.props.skills.map(function(skill, index){
+							console.log('skill'+target+index);
+							console.log("skill",skill);
+							return li({key: 'skill'+target+index},
+								( skill.icon ?
+									span({key: 'iconspan'+target+index, className: 'onethird'},
+										em({
+											className: skill.icon,
+											style: {color: skill.color}
+										})
+									)
+								:
+									span({key: 'iconspan'+target+index, className: 'onethird'},
+										img({
+											className: skill.className, 
+											src:skill.img, 
+											alt: skill.text
+										})
+									)
+								),
+								span({key: 'textspan'+target+index, className: 'twothird'},
+									skill.text
+								),
+								(skill.subSkills ? 
+									ul({key:'subskillsUl'+target+index},
+										skill.subSkills.map(function(subSkill, index){
+											return li(null,
+												( subSkill.icon ?
+													span({className: 'onethird'},
+														em({
+															className: subSkill.icon,
+															style: {color: subSkill.color}
+														})
+													)
+												:
+													span({className: 'onethird'},
+														img({
+															className: subSkill.className, 
+															src:subSkill.img, 
+															alt: subSkill.text
+														})
+													)							
+												),
+												span({className: 'twothird'},
+													subSkill.text
+												)
+											)
+										})
+									)
+								: null
+								)
+							)
+						})
+					)
+				)
+			)
+		}
+	})
+
+	module.exports = AboutTechnicalSkills;
 
 /***/ }
 /******/ ]);
