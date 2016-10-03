69ce0fa8454a15f45aa4a407f9c151f320058c7c
Carol St.Louis <stlouis_c@yahoo.com>
2016-10-03 11:46:39 -0700

    added placeholder Tribute section Component

---
+++ src/Tribute.js
@@ -0,0 +1,11 @@
+const React = require('react');
+
+const Tribute = () => (
+	<section className='tribute'>
+		<div className='container'>
+			<p>Tribute page here</p>
+		</div>
+	</section>
+);
+
+module.exports = Tribute;
