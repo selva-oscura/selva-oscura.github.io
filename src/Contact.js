cadcdae1a2bdda93ef445a0d6f58242f687b8b6b
Carol St.Louis <stlouis_c@yahoo.com>
2016-10-03 11:46:24 -0700

    added placeholder Contact section Component

---
+++ src/Contact.js
@@ -0,0 +1,11 @@
+const React = require('react');
+
+const Contact = () => (
+	<section className='contact'>
+		<div className='container'>
+			<p>Contact here</p>
+		</div>
+	</section>
+)
+
+module.exports = Contact;
