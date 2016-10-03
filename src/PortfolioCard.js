d81d057075c2c90456b14cf7a7191c482b3901ae
Carol St.Louis <stlouis_c@yahoo.com>
2016-10-02 21:28:35 -0700

    converted PortfolioCard Component to jsx

--- src/PortfolioCard.js
+++ src/PortfolioCard.js
@@ -1,31 +1,12 @@
 var React = require('react');
-var img = React.DOM.img;
-var div = React.DOM.div;
-var h3 = React.DOM.h3;
-var p = React.DOM.p;
-var strong = React.DOM.strong;
+const PortfolioCardImage = require('./PortfolioCardImage.js');
+const PortfolioCardText = require('./PortfolioCardText.js');
 
-var PortfolioCardImage = require('./PortfolioCardImage.js');
-var PortfolioCardText = require('./PortfolioCardText.js');
-
-var PortfolioCard = React.createClass({
-	render(){
-		return(
-			div({className: 'card'},
-				React.createElement(PortfolioCardImage, {
-					src:this.props.src,
-					name:this.props.name
-				}),
-				React.createElement(PortfolioCardText,{					
-					href: this.props.href,
-					target: this.props.target,
-					name:this.props.name,
-					text: this.props.text,
-					technologies: this.props.technologies
-				})
-			)
-		)
-	}
-});
+const PortfolioCard = (props) => (
+	<div className='card'>
+		<PortfolioCardImage src={props.src} name={props.name} />
+		<PortfolioCardText href={props.href} target={props.target} name={props.name} text={props.text} technologies={props.technologies} />
+	</div>
+)
 
 module.exports = PortfolioCard;
