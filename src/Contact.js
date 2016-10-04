39b5cdaaf65ae40d54dad49c98a81451c4c39588
Carol St.Louis <stlouis_c@yahoo.com>
2016-10-03 22:02:22 -0700

    roughed out template for layout of Contact Component and data for creating the Contact Form Component

--- src/Contact.js
+++ src/Contact.js
@@ -1,11 +1,47 @@
 const React = require('react');
 
-const Contact = () => (
-	<section className='contact'>
-		<div className='container'>
-			<p>Contact here</p>
-		</div>
-	</section>
-)
+const Contact = React.createClass({
+	render: function(){
+		var formFields = [
+			{
+				type: 'text',
+				text: 'Name',
+				name: 'name',
+				required: true
+			},			{
+				type: 'email',
+				text: 'E-Mail Address',
+				name: 'email',
+				required: true
+			},			{
+				type: 'tel',
+				text: 'Telephone Number',
+				name: 'phone',
+				required: true
+			},			{
+				type: 'textarea',
+				text: 'Message',
+				name: 'message',
+				required: true
+			},			{
+				type: 'submit',
+				text: 'SUBMIT',
+				name: 'submit',
+				required: 
+			}
+		]
+		return (
+			<section className='contact'>
+				<div className='container'>
+					<div className='half'>
+						<form></form>
+					</div>
+					<div className='half'>
+					</div>
+				</div>
+			</section>
+		)		
+	}
+})
 
 module.exports = Contact;
