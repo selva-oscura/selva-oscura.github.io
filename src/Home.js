6fb4780a984c316d2a00c0eb4e904c6b4dd385fb
Carol St.Louis <stlouis_c@yahoo.com>
2016-10-08 22:51:01 -0700

    changed background and layout of button/links on home section; forced scrollbars for all pages to prevent navbar jump;

--- src/Home.js
+++ src/Home.js
@@ -3,11 +3,15 @@ const React = require('react');
 const Home = () => (
 	<section className='home'>
 		<div className='container'>
-			<div className='social' style={{paddingTop: '66vh'}}>
-				<p>
-					<a href='https://github.com/selva-oscura' target='github'>GitHub</a>
-					<a href='https://linkedin.com/in/stlouisc' target='linkedIn'>LinkedIn</a>
-				</p>
+			<div className='half'>
+				<div className='social' style={{paddingTop: '50vh'}}>
+					<p>
+						<a href='https://github.com/selva-oscura' target='github'>GitHub</a>
+					</p>
+					<p>
+						<a href='https://linkedin.com/in/stlouisc' target='linkedIn'>LinkedIn</a>
+					</p>
+				</div>
 			</div>
 		</div>
 	</section>
