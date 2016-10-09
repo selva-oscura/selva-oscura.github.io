4604a57d8277db272adfbda6657eaac62d2a0fbb
Carol St.Louis <stlouis_c@yahoo.com>
2016-10-08 20:06:29 -0700

    added href parameters to NavBar data, so each panel now is own page

--- src/NavBar.js
+++ src/NavBar.js
@@ -6,9 +6,9 @@ const NavBar = () => (
 		<div className='container'>
 			<ul>
 				<NavLink className='navscroll active' id='home' href='/' text='HOME' />
-				<NavLink className='navscroll' id='about' href='/' text='ABOUT' />
-				<NavLink className='navscroll' id='portfolio' href='/' text='PORTFOLIO' />
-				<NavLink className='navscroll' id='contact' href='/' text='CONTACT' />
+				<NavLink className='navscroll' id='about' href='/about' text='ABOUT' />
+				<NavLink className='navscroll' id='portfolio' href='/portfolio' text='PORTFOLIO' />
+				<NavLink className='navscroll' id='contact' href='/contact' text='CONTACT' />
 				<NavLink className='navscroll' id='tribute' href='/tribute' text='TRIBUTE' />
 			</ul>
 		</div>
