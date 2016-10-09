48ad85406db33e623c55e63400ac7f2d10e60ef8
Carol St.Louis <stlouis_c@yahoo.com>
2016-10-08 20:08:00 -0700

    added routing for About, Contact, Portfolio, and change route name for Home (former index)

--- src/app.js
+++ src/app.js
@@ -4,13 +4,19 @@ const ReactRouter = require('react-router');
 const { Router, Route, hashHistory } = ReactRouter;
 
 const NavBar = require('./NavBar.js');
-const IndexPage = require('./IndexPage.js')
+const HomePage = require('./HomePage.js')
+const AboutPage = require('./AboutPage.js')
+const PortfolioPage = require('./PortfolioPage.js')
+const ContactPage = require('./ContactPage.js')
 const TributePage = require('./TributePage.js');
 
 const App = () => (
 
   <Router history={hashHistory}>
-    <Route path='/' component={IndexPage} />
+    <Route path='/' component={HomePage} />
+    <Route path='/about' component={AboutPage} />
+    <Route path='/portfolio' component={PortfolioPage} />
+    <Route path='/contact' component={ContactPage} />
     <Route path='/tribute' component={TributePage} />
   </Router>
 )
