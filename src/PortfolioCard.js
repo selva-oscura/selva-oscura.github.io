f520b0135f684e338acb19873b7fae79d4876a56
Carol St.Louis <stlouis_c@yahoo.com>
2016-10-11 22:59:21 -0700

    changing layout to vertical cards

--- src/PortfolioCard.js
+++ src/PortfolioCard.js
@@ -1,12 +1,15 @@
 var React = require('react');
-const PortfolioCardImage = require('./PortfolioCardImage.js');
-const PortfolioCardText = require('./PortfolioCardText.js');
+// const PortfolioCardImage = require('./PortfolioCardImage.js');
+// const PortfolioCardText = require('./PortfolioCardText.js');
 
 const PortfolioCard = (props) => (
-	<div className='card'>
-		<PortfolioCardImage src={props.src} name={props.name} />
-		<PortfolioCardText href={props.href} target={props.target} name={props.name} text={props.text} technologies={props.technologies} />
+	<div className="card cardModalTrigger xs1 s2 m3 l4" id={props.target}>
+		<img src={props.src} alt={props.name} />
+		<div className="card-container">
+			<h4><b>{props.name}</b></h4> 
+			<p><strong>Technologies: </strong>{props.technologies}</p> 
+		</div>
 	</div>
-)
+);
 
 module.exports = PortfolioCard;
