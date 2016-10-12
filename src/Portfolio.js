f7eb165461a2e2d94336bf93f793068fb7442ee3
Carol St.Louis <stlouis_c@yahoo.com>
2016-10-11 22:58:55 -0700

    added ES6 to Shines

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -11,7 +11,7 @@ const Portfolio = React.createClass({
 			href: 'https://doortodoorshines.com',
 			target: 'shines',
 			text: ["Door-to-Door Shines is an app to help busy people in the San Francisco Area with on-demand shoe shine and repair services.  Select the services you want, schedule pick-up and return times that are convenient for you, pay, and cross one more errand off your list!","As sole developer for Door-to-Door Shines, I built (1) the interfaces the user sees as s/he orders shine & repair services, schedules pick-up & delivery times, pays, leaves comments or asks questions; (2) the admin dashboard, displaying pending & completed orders, user feedback, & customer information, scheduling of delivery times, analytics on orders and revenue; and (3) the internals, including database and cron jobs."],
-			technologies: "Meteor.js (Blaze, Spacebars, MongoDB, Node.js), JavaScript, jQuery, HTML5, CSS3, Materialize.css, Stripe API"
+			technologies: "Meteor.js (Blaze, Spacebars, MongoDB, Node.js), JavaScript/ES6, jQuery, HTML5, CSS3, Materialize.css, Stripe API"
 		},{
 			src:"public/img/zyrl.png",
 			name:"Zyrl",
