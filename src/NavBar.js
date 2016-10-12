bb2bdeef4f1ca8200652e9b5776c311082ea325f
Carol St.Louis <stlouis_c@yahoo.com>
2016-10-11 22:58:25 -0700

    removed comments

--- src/NavBar.js
+++ src/NavBar.js
@@ -12,9 +12,7 @@ const NavBar = (props) => {
 	var linkList = [];
 	links.forEach(function(link, i){
 		var className;
-		console.log('props.active', props.active)
 		if (link.id===props.active){ className = "active"; }
-		console.log('className',className);
 		linkList.push(<NavLink key={i} id={link.id} href={link.href} text={link.text} className={className} />);
 	});
 	return (
