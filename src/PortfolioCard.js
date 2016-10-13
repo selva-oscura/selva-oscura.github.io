8f2a2bcb6d639e9af6a7d8d9a7b058fad01c9aeb
Carol St.Louis <stlouis_c@yahoo.com>
2016-10-12 20:28:35 -0700

    changed layout of card

--- src/PortfolioCard.js
+++ src/PortfolioCard.js
@@ -2,14 +2,18 @@ var React = require('react');
 // const PortfolioCardImage = require('./PortfolioCardImage.js');
 // const PortfolioCardText = require('./PortfolioCardText.js');
 
-const PortfolioCard = (props) => (
-	<div className="card cardModalTrigger xs1 s2 m3 l4" id={props.target}>
-		<img src={props.src} alt={props.name} />
-		<div className="card-container">
-			<h4><b>{props.name}</b></h4> 
-			<p><strong>Technologies: </strong>{props.technologies}</p> 
+const PortfolioCard = (props) => {
+	return(
+		<div className="card-container xs1 s2 m3 l4">
+			<div className="card cardModalTrigger" id={props.target}>
+				<img src={props.src} className="card-image" alt={props.name} />
+				<div className="card-text-space">
+					<h4><b>{props.name}</b></h4> 
+					<p><strong>Technologies: </strong>{props.technologies}</p> 
+				</div>
+			</div>
 		</div>
-	</div>
-);
+	)
+};
 
 module.exports = PortfolioCard;
