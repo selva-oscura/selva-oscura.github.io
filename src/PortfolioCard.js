dd9f2eaa3e9dffdb52fa9b802b065011d9368d0e
Carol St.Louis <stlouis_c@yahoo.com>
2016-10-13 20:45:25 -0700

    removed className card-image in favour of just using .card img

--- src/PortfolioCard.js
+++ src/PortfolioCard.js
@@ -6,7 +6,7 @@ const PortfolioCard = (props) => {
 	return(
 		<div className="card-container xs1 s2 m3 l4">
 			<div className="card cardModalTrigger" id={props.target}>
-				<img src={props.src} className="card-image" alt={props.name} />
+				<img src={props.src} alt={props.name} />
 				<div className="card-text-space">
 					<h4><b>{props.name}</b></h4> 
 					<p><strong>Technologies: </strong>{props.technologies}</p> 
