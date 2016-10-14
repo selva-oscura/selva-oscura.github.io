7a27cf2973b5db0f5678c6adefdc6a6542a4a818
Carol St.Louis <stlouis_c@yahoo.com>
2016-10-13 21:30:33 -0700

    updated bundle

--- public/bundle.js
+++ public/bundle.js
@@ -58,8 +58,8 @@
 	var HomePage = __webpack_require__(224);
 	var AboutPage = __webpack_require__(226);
 	var PortfolioPage = __webpack_require__(229);
-	var ContactPage = __webpack_require__(232);
-	var TributePage = __webpack_require__(235);
+	var ContactPage = __webpack_require__(233);
+	var TributePage = __webpack_require__(236);
 
 	var App = function App() {
 	  return React.createElement(
@@ -26234,6 +26234,7 @@
 	var React = __webpack_require__(1);
 
 	var PortfolioCard = __webpack_require__(231);
+	var PortfolioModal = __webpack_require__(232);
 
 	var Portfolio = React.createClass({
 		displayName: 'Portfolio',
@@ -26242,49 +26243,59 @@
 			var projects = [{
 				src: "public/img/shines.png",
 				name: "Door-to-Door Shines",
-				href: 'https://doortodoorshines.com',
+				web: 'https://doortodoorshines.com',
 				target: 'shines',
 				text: ["Door-to-Door Shines is an app to help busy people in the San Francisco Area with on-demand shoe shine and repair services.  Select the services you want, schedule pick-up and return times that are convenient for you, pay, and cross one more errand off your list!", "As sole developer for Door-to-Door Shines, I built (1) the interfaces the user sees as s/he orders shine & repair services, schedules pick-up & delivery times, pays, leaves comments or asks questions; (2) the admin dashboard, displaying pending & completed orders, user feedback, & customer information, scheduling of delivery times, analytics on orders and revenue; and (3) the internals, including database and cron jobs."],
 				technologies: "Meteor.js, MongoDB, Node.js, JavaScript/ES6, jQuery, HTML5, CSS3, Materialize.css, moment.js, meteor-synced-cron, Stripe API"
 			}, {
 				src: "public/img/zyrl.png",
 				name: "Zyrl",
-				href: 'https://zyrl.meteorapp.com',
+				web: 'https://zyrl.meteorapp.com',
 				target: 'zyrl',
 				text: ["Zyrl is an app for connecting social media influencers and the merchants whose products and services they love.", "As a freelancer for Zyrl, I converted the site from WordPress to Meteor, rebuilding the app's front-end and back-end (sign-up and communications for influencers, merchants, and potential employees) and arranging site and database hosting. I then managed the Facebook and Instagram permissions process, designed and developed the internal APIs & the management of data from Facebook and Instagram's APIs, and designed the admin dashboard for managing influencers/merchants, admin permissions, and display of analytics of  influencers' Facebook and Instagram profiles and posts data."],
 				technologies: "Meteor.js, MongoDB, Node.js, JavaScript/ES6, jQuery, HTML5, CSS3, Bootstrap, Facebook Graph API, Instagram API"
 			}, {
+				src: 'public/img/portfolio.png',
+				name: 'Portfolio Page',
+				git: 'https://github.com/selva-oscura/selva-oscura',
+				web: 'https://selva-oscura.github.io/',
+				target: 'portfolio',
+				text: ["This is the portfolio page at which you are currently looking. As of 10/13/2016, it is a work in progressing, starting as a vanilla html site (based on the Free Code Camp portfolio project).  It is undergoing repeated iterations as it is converted to React, given routes....and yet more changes coming."],
+				technologies: 'React.js, JavaScript / ES6, jQuery, HTML5, CSS3, normalize.css'
+			}, {
 				src: 'public/img/portcal.jpg',
 				name: 'PoRTCal',
-				href: 'https://github.com/selva-oscura/poRTCal',
+				git: 'https://github.com/selva-oscura/poRTCal',
 				target: 'portcal',
 				text: ["PoRTCal is a WebRTC app inspired by the game Portal. The project was an entry into the October 2015 Meteor hackathon competition. Users could videoconference with each other by logging in to the app."],
 				technologies: 'Meteor.js, MongoDB, Node.js, HTML5, CSS3, Materialize.css, Javascript/ES6, WebRTC using the PeerJS library'
 			}, {
 				src: "public/img/animal_rescue.png",
 				name: "Animal Rescue",
-				href: 'https://github.com/selva-oscura/meteor_animal_rescue',
+				git: 'https://github.com/selva-oscura/meteor_animal_rescue',
 				target: 'animal_rescue',
 				text: ["Animal Rescue is an app for uploading and finding animals that are available for adoption, make with Meteor, MongoDB, and Bootstrap. This project was developed and won 2nd place at the Women Who Code Silicon Valley hackathon at Paypal in April 2015."],
 				technologies: "Meteor.js, MongoDB, Node.js, HTML5, CSS3, Bootstrap, MapQuest Geolocation API"
 			}, {
 				src: "public/img/booksmart.png",
 				name: "Booksmart",
-				href: 'http://booksmartapp.com',
+				web: 'http://booksmartapp.com',
 				target: 'booksmart',
 				text: ["Sole developer and lead maintainer of the web platform for a web, iOS, and Android project, created in collaboration with a team of mobile app developers to help students (1) contact other students at their university to buy or sell textbooks with one another and (2) buy from or sell to online vendors for students at universities where the app is not yet broadly adopted."],
 				technologies: "HTM5, CSS3, Bootstrap, PHP, MySQL, Javascript, AJAX, jQuery, Campusbooks API"
 			}, {
 				src: "public/img/aloha_fun_zone.png",
 				name: "Aloha Fun Zone",
-				href: 'http://alohafunzone.com',
+				web: 'http://alohafunzone.com',
 				target: 'aloha',
 				text: ["Sole developer and maintainer of Aloha Fun Zone website, which displays the tours available from the company."],
 				technologies: "HTM5, CSS3, PHP, Javascript, AJAX, jQuery"
 			}];
 			var cards = [];
+			var modals = [];
 			projects.forEach(function (project) {
 				cards.push(React.createElement(PortfolioCard, { src: project.src, name: project.name, href: project.href, target: project.target, text: project.text, technologies: project.technologies, key: project.target }));
+				modals.push(React.createElement(PortfolioModal, { name: project.name, web: project.web, repo: project.git, target: project.target, text: project.text, technologies: project.technologies, key: project.target + '_modal' }));
 			});
 			return React.createElement(
 				'section',
@@ -26293,7 +26304,8 @@
 				React.createElement(
 					'div',
 					{ className: 'container' },
-					cards
+					cards,
+					modals
 				)
 			);
 		}
@@ -26318,7 +26330,7 @@
 			React.createElement(
 				"div",
 				{ className: "card cardModalTrigger", id: props.target },
-				React.createElement("img", { src: props.src, className: "card-image", alt: props.name }),
+				React.createElement("img", { src: props.src, alt: props.name }),
 				React.createElement(
 					"div",
 					{ className: "card-text-space" },
@@ -26352,6 +26364,83 @@
 /* 232 */
 /***/ function(module, exports, __webpack_require__) {
 
+	"use strict";
+
+	var React = __webpack_require__(1);
+	// const PortfolioCardImage = require('./PortfolioCardImage.js');
+	// const PortfolioCardText = require('./PortfolioCardText.js');
+
+	var PortfolioModal = function PortfolioModal(props) {
+		var modalClass = "modal " + props.target;
+		return React.createElement(
+			"div",
+			{ className: "modal-content" },
+			React.createElement(
+				"div",
+				{ className: "modal-header" },
+				React.createElement(
+					"span",
+					{ className: "close" },
+					"x"
+				),
+				React.createElement(
+					"h2",
+					null,
+					props.name
+				)
+			),
+			React.createElement(
+				"div",
+				{ className: "modal-body" },
+				React.createElement(
+					"p",
+					null,
+					props.text
+				),
+				React.createElement(
+					"p",
+					null,
+					React.createElement(
+						"strong",
+						null,
+						"Technologies: "
+					),
+					props.technologies
+				)
+			),
+			React.createElement(
+				"div",
+				{ className: "modal-footer" },
+				React.createElement(
+					"div",
+					{ className: "half" },
+					props.web ? React.createElement(
+						"a",
+						{ href: props.web, target: props.target },
+						"Project Website"
+					) : " "
+				),
+				React.createElement(
+					"div",
+					{ className: "half text-right" },
+					props.repo ? React.createElement(
+						"a",
+						{ href: props.repo, target: props.target },
+						"Project Repo"
+					) : " "
+				)
+			)
+		);
+	};
+
+	module.exports = PortfolioModal;
+	// <div className={modalClass}>
+	// </div>
+
+/***/ },
+/* 233 */
+/***/ function(module, exports, __webpack_require__) {
+
 	'use strict';
 
 	var React = __webpack_require__(1);
@@ -26360,7 +26449,7 @@
 
 
 	var NavBar = __webpack_require__(222);
-	var Contact = __webpack_require__(233);
+	var Contact = __webpack_require__(234);
 
 	var ContactPage = function ContactPage() {
 		return React.createElement(
@@ -26374,13 +26463,13 @@
 	module.exports = ContactPage;
 
 /***/ },
-/* 233 */
+/* 234 */
 /***/ function(module, exports, __webpack_require__) {
 
 	'use strict';
 
 	var React = __webpack_require__(1);
-	var FormInput = __webpack_require__(234);
+	var FormInput = __webpack_require__(235);
 
 	var Contact = React.createClass({
 		displayName: 'Contact',
@@ -26449,7 +26538,7 @@
 	module.exports = Contact;
 
 /***/ },
-/* 234 */
+/* 235 */
 /***/ function(module, exports, __webpack_require__) {
 
 	'use strict';
@@ -26483,7 +26572,7 @@
 	module.exports = FormInput;
 
 /***/ },
-/* 235 */
+/* 236 */
 /***/ function(module, exports, __webpack_require__) {
 
 	'use strict';
@@ -26494,7 +26583,7 @@
 
 
 	var NavBar = __webpack_require__(222);
-	var Tribute = __webpack_require__(236);
+	var Tribute = __webpack_require__(237);
 
 	var TributePage = function TributePage() {
 		return React.createElement(
@@ -26508,13 +26597,13 @@
 	module.exports = TributePage;
 
 /***/ },
-/* 236 */
+/* 237 */
 /***/ function(module, exports, __webpack_require__) {
 
 	'use strict';
 
 	var React = __webpack_require__(1);
-	var Base = __webpack_require__(237);
+	var Base = __webpack_require__(238);
 
 	var Tribute = React.createClass({
 		displayName: 'Tribute',
@@ -26807,7 +26896,7 @@
 	module.exports = Tribute;
 
 /***/ },
-/* 237 */
+/* 238 */
 /***/ function(module, exports, __webpack_require__) {
 
 	'use strict';
