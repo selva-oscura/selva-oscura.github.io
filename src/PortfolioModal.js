734a9906a795ea5982dbec03490a870005e820ef
Carol St.Louis <stlouis_c@yahoo.com>
2016-10-15 00:54:11 -0700

    created basic PortfolioModal Component and event handler to close modal on click of x

--- src/PortfolioModal.js
+++ src/PortfolioModal.js
@@ -1,31 +1,34 @@
 var React = require('react');
-// const PortfolioCardImage = require('./PortfolioCardImage.js');
-// const PortfolioCardText = require('./PortfolioCardText.js');
 
-const PortfolioModal = (props) => {
-	var modalClass = "modal " + props.target;
-	return(
-			<div className="modal-content">
-				<div className="modal-header">
-					<span className="close">x</span>
-					<h2>{props.name}</h2>
-				</div>
-				<div className="modal-body">
-					<p>{props.text}</p>
-					<p><strong>Technologies: </strong>{props.technologies}</p> 
-				</div>
-				<div className="modal-footer">
-					<div className="half">
-						{ props.web ? <a href={props.web} target={props.target}>Project Website</a> : " " }
+const PortfolioModal = React.createClass({
+	hideModal: (target) => {
+		var targeted = document.getElementById(target);
+		targeted.style.display = "none";
+	},
+	render: function(){	
+		return (
+			<div className="modal" id={this.props.target}>
+				<div className="modal-content">
+					<div className="modal-header">
+						<span className="modal-close" onClick={this.hideModal.bind(this,this.props.target)}>x</span>
+						<h2>{this.props.name}</h2>
 					</div>
-					<div className="half text-right">
-						{ props.repo ? <a href={props.repo} target={props.target}>Project Repo</a> : " " }
+					<div className="modal-body">
+						<p>{this.props.text}</p>
+						<p><strong>Technologies: </strong>{this.props.technologies}</p> 
+					</div>
+					<div className="modal-footer">
+						<div className="half">
+							{ this.props.web ? <a href={this.props.web} target={this.props.target}>Project Website</a> : " " }
+						</div>
+						<div className="half text-right">
+							{ this.props.repo ? <a href={this.props.repo} target={this.props.target}>Project Repo</a> : " " }
+						</div>
 					</div>
 				</div>
 			</div>
-	)
-};
+		)
+	}
+});
 
 module.exports = PortfolioModal;
-		// <div className={modalClass}>
-		// </div>
