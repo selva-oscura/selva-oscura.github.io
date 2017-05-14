482f74686178c8e015f2591df82fc12cd3f946d7
Carol St.Louis <1.selva.oscura@gmail.com>
2017-05-14 00:01:29 -0700

    corrected url

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -8,7 +8,7 @@ const Portfolio = React.createClass({
 		const projects=[{
 			src:"public/img/shines.png",
 			name:"Door-to-Door Shines",
-			web: 'https://doortodoorshines.com',
+			web: 'https://www.doortodoorshines.com',
 			target: 'shines',
 			text: ["Door-to-Door Shines is an app to help busy people in the San Francisco Area with on-demand shoe shine and repair services.  Select the services you want, schedule pick-up and return times that are convenient for you, pay, and cross one more errand off your list!","As sole developer for Door-to-Door Shines, I built (1) the interfaces the user sees as s/he orders shine & repair services, schedules pick-up & delivery times, pays, leaves comments or asks questions; (2) the admin dashboard, displaying pending & completed orders, user feedback, & customer information, scheduling of delivery times, analytics on orders and revenue; and (3) the internals, including database and cron jobs."],
 			technologies: "Meteor.js, MongoDB, Node.js, JavaScript/ES6, jQuery, HTML5, CSS3, Materialize.css, moment.js, meteor-synced-cron, Stripe API"
@@ -25,7 +25,7 @@ const Portfolio = React.createClass({
 			git: 'https://github.com/selva-oscura/selva-oscura',
 			web: 'https://selva-oscura.github.io/',
 			target: 'portfolio',
-			text: ["This is the portfolio page at which you are currently looking. As of 10/13/2016, it is a work in progressing, starting as a vanilla html site (based on the Free Code Camp portfolio project).  It is undergoing repeated iterations as it is converted to React, given routes....and yet more changes coming."],
+			text: ["This is the portfolio page at which you are currently looking. As of 10/13/2016, it is a work in progress, starting as a vanilla html site (based on the Free Code Camp portfolio project).  It is undergoing repeated iterations as it is converted to React, given routes....and yet more changes coming."],
 			technologies: 'React.js, JavaScript / ES6, jQuery, HTML5, CSS3, normalize.css'
 		},{
 			src: 'public/img/portcal.jpg',
