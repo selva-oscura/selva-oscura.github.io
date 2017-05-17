ce7946a37a39c1f781c75c6f40d6e5431f32d7ee
Carol St.Louis <1.selva.oscura@gmail.com>
2017-05-16 19:41:07 -0700

    created index to organize import of containers

---
+++ src/components/index.js
@@ -0,0 +1,16 @@
+import Contact from './Contact';
+import Footer from './Footer';
+import Header from './Header';
+import Main from './Main';
+import Portfolio from './Portfolio';
+import Project from './Project';
+import ProjectModals from './ProjectModals';
+
+export {
+	Contact,
+	Footer,
+	Header,
+	Main,
+	Portfolio,
+	ProjectModals,
+}
