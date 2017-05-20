a8be17cbc66421523247c964bfef24a664a8e8fa
Carol St.Louis <1.selva.oscura@gmail.com>
2017-05-19 22:39:14 -0700

    replaced # with dummy urls (to be replaced later)

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,11 +5,11 @@ const Footer = () => {
 		<footer id="footer">
 
 			<ul className="icons">
-				<li><a href="#" className="icon circle fa-twitter"><span className="label">Twitter</span></a></li>
-				<li><a href="#" className="icon circle fa-facebook"><span className="label">Facebook</span></a></li>
-				<li><a href="#" className="icon circle fa-google-plus"><span className="label">Google+</span></a></li>
-				<li><a href="#" className="icon circle fa-github"><span className="label">Github</span></a></li>
-				<li><a href="#" className="icon circle fa-dribbble"><span className="label">Dribbble</span></a></li>
+				<li><a href="https://must.be.filled.in" className="icon circle fa-twitter"><span className="label">Twitter</span></a></li>
+				<li><a href="https://must.be.filled.in" className="icon circle fa-facebook"><span className="label">Facebook</span></a></li>
+				<li><a href="https://must.be.filled.in" className="icon circle fa-google-plus"><span className="label">Google+</span></a></li>
+				<li><a href="https://must.be.filled.in" className="icon circle fa-github"><span className="label">Github</span></a></li>
+				<li><a href="https://must.be.filled.in" className="icon circle fa-dribbble"><span className="label">Dribbble</span></a></li>
 			</ul>
 
 			<ul className="copyright">
