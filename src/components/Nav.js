673fdc0f314e4dbead7b42183fddd83181c16a71
Carol St.Louis <1.selva.oscura@gmail.com>
2017-05-21 22:53:57 -0700

    moved hard-coded navbar from App to Nav

---
+++ src/components/Nav.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Nav = () => {
+	return (
+		<nav id="nav">
+			<ul className="container">
+				<li><a href="#home">Home</a></li>
+				<li><a href="#skills">Skills</a></li>
+				<li><a href="#portfolio">Portfolio</a></li>
+				<li><a href="#contact">Contact Me</a></li>
+			</ul>
+		</nav>
+	)
+}
+
+export default Nav;
