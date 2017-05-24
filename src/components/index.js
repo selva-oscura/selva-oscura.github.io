2de51398d40119e03063714e528395f414345346
Carol St.Louis <1.selva.oscura@gmail.com>
2017-05-23 20:19:34 -0700

    imported and exported Portfolio and Contact

--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,13 @@
 import Nav from './Nav';
 import Home from './Home';
 import Profile from './Profile';
+import Portfolio from './Portfolio';
+import Contact from './Contact';
 
 export {
 	Nav,
 	Home,
 	Profile,
+	Portfolio,
+	Contact,
 }
