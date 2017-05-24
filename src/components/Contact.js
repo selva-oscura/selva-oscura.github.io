1d42a452569036a8970a4b2e1bee1b485b0ea512
Carol St.Louis <1.selva.oscura@gmail.com>
2017-05-23 20:26:27 -0700

    moved hard-coded social media links from Contact to FindMe

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import FindMe from './FindMe';
 
 const Contact = () => {
 	return (
@@ -43,22 +44,9 @@ const Contact = () => {
 							</form>
 						</div>
 					</div>
-					<div className="row">
-						<div className="12u">
-							<hr />
-							<h3>Find me on ...</h3>
-							<ul className="social">
-								<li><a href="blah" className="icon fa-twitter"><span className="label">Twitter</span></a></li>
-								<li><a href="blah" className="icon fa-facebook"><span className="label">Facebook</span></a></li>
-								<li><a href="blah" className="icon fa-dribbble"><span className="label">Dribbble</span></a></li>
-								<li><a href="blah" className="icon fa-linkedin"><span className="label">LinkedIn</span></a></li>
-								<li><a href="blah" className="icon fa-tumblr"><span className="label">Tumblr</span></a></li>
-								<li><a href="blah" className="icon fa-google-plus"><span className="label">Google+</span></a></li>
-								<li><a href="blah" className="icon fa-github"><span className="label">Github</span></a></li>
-							</ul>
-							<hr />
-						</div>
-					</div>
+
+					<FindMe />
+					
 				</div>
 				<footer>
 					<ul id="copyright">
