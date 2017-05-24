1a5e0b7ba694763ee819912633335a50578c4966
Carol St.Louis <1.selva.oscura@gmail.com>
2017-05-23 20:46:36 -0700

    moved hard-coded copyright into Copyright

---
+++ src/components/Copyright.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Copyright = () => {
+	return (
+		<footer>
+			<ul id="copyright">
+				<li>&copy; Untitled. All rights reserved.</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
+			</ul>
+		</footer>
+	)
+}
+
+export default Copyright;
