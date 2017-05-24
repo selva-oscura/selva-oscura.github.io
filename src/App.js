a615233a8aeee6847a338834abb100c101e77418
Carol St.Louis <1.selva.oscura@gmail.com>
2017-05-23 23:03:57 -0700

    made updateFormState and submitMailForm live; added submitting to state.form; passed state.form, updateFormState, and submitMailForm to Contact

--- src/App.js
+++ src/App.js
@@ -14,62 +14,66 @@ class App extends Component {
 				message: "",
 				responses: [],
 				errors: [],
+				submitting: false,
 			}
 		}
-		console.log('this.state', this.state);
-		// this.updateFormState = this.updateFormState.bind(this);
-		// this.submitMailForm = this.submitMailForm.bind(this);
+		this.updateFormState = this.updateFormState.bind(this);
+		this.submitMailForm = this.submitMailForm.bind(this);
 	}
-	// updateFormState(e){
-	// 	let form = this.state.form;
-	// 	if(e.target.type === "checkbox"){
-	// 		form[e.target.id]=!form[e.target.id];
-	// 	}else if(e.target.type==="radio"){
-	// 		form[e.target.name]=e.target.id;
-	// 	}else{
-	// 		form[e.target.id]=e.target.value;
-	// 	}
-	// 	form.responses=[];
-	// 	form.errors=[];
-	// 	this.setState({form});
-	// }
-	// submitMailForm(e){
-	// 	e.preventDefault();
-	// 	let form = this.state.form;
-	// 	let emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-	// 	if(form.name.length===0){
-	// 		form.errors.push("Missing Name");
-	// 	}
-	// 	if(form.email.length===0){
-	// 		form.errors.push("Missing E-Mail Address");
-	// 	}else if(!form.email.match(emailRegex)){
-	// 		form.errors.push("Improperly-Formatted E-Mail Address");
-	// 	}
-	// 	if(form.subject.length===0){
-	// 		form.errors.push("Missing Subject");
-	// 	}
-	// 	if(form.message.length===0){
-	// 		form.errors.push("Missing Message");
-	// 	}
-	// 	if(form.errors.length===0){
-	// 		form.errors = [];
-	// 		form.responses.push("Submitting....");
-	// 		form.responses.push(`Name: ${form.name}`);
-	// 		form.responses.push(`E-Mail: ${form.email}`);
-	// 		form.responses.push(`Message: ${form.message}`);
-	// 		form.name = "";
-	// 		form.email = "";
-	// 		form.message = "";
-	// 		form.subject = "";
-	// 	}else{
-	// 		if(form.errors.length===1){
-	// 			form.errors.unshift("Can Not Submit the Form, Due to the Following Problem:");
-	// 		}else{
-	// 			form.errors.unshift("Can Not Submit the Form, Due to the Following Problems:");
-	// 		}
-	// 	}
-	// 	this.setState({form});
-	// }
+	updateFormState(e){
+		console.log('change detected')
+		let form = this.state.form;
+		if(e.target.type === "checkbox"){
+			form[e.target.id]=!form[e.target.id];
+		}else if(e.target.type==="radio"){
+			form[e.target.name]=e.target.id;
+		}else{
+			form[e.target.id]=e.target.value;
+		}
+		form.responses=[];
+		form.errors=[];
+		this.setState({form});
+		console.log(this.state.form)
+	}
+	submitMailForm(e){
+		e.preventDefault();
+		let form = this.state.form;
+		let emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+		if(form.name.length===0){
+			form.errors.push("Missing Name");
+		}
+		if(form.email.length===0){
+			form.errors.push("Missing E-Mail Address");
+		}else if(!form.email.match(emailRegex)){
+			form.errors.push("Improperly-Formatted E-Mail Address");
+		}
+		if(form.subject.length===0){
+			form.errors.push("Missing Subject");
+		}
+		if(form.message.length===0){
+			form.errors.push("Missing Message");
+		}
+		if(form.errors.length===0){
+			form.errors = [];
+			form.responses.push("Submitting....");
+			form.responses.push(`Name: ${form.name}`);
+			form.responses.push(`E-Mail: ${form.email}`);
+			form.responses.push(`Message: ${form.message}`);
+			form.name = "";
+			form.email = "";
+			form.message = "";
+			form.subject = "";
+			form.submitting = true;
+		}else{
+			if(form.errors.length===1){
+				form.errors.unshift("Can Not Submit the Form, Due to the Following Problem:");
+			}else{
+				form.errors.unshift("Can Not Submit the Form, Due to the Following Problems:");
+			}
+		}
+		this.setState({form});
+		console.log(this.state.form)
+	}
 	render(){
 		return(
 			<div>
@@ -82,7 +86,11 @@ class App extends Component {
 
 				<Portfolio />
 
-				<Contact />
+				<Contact
+					form={this.state.form}
+					updateFormState={this.updateFormState}
+					submitMailForm={this.submitMailForm}
+				/>
 
 			</div>
 		)
