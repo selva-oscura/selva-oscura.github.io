8ff6ca4ffae43c48b6662d6bedf29322318dd6fc
Carol St.Louis <1.selva.oscura@gmail.com>
2017-05-23 23:22:49 -0700

    added clearing of errors at beginning of submitMailForm

--- src/App.js
+++ src/App.js
@@ -38,6 +38,7 @@ class App extends Component {
 	submitMailForm(e){
 		e.preventDefault();
 		let form = this.state.form;
+		form.errors = [];
 		let emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 		if(form.name.length===0){
 			form.errors.push("Missing Name");
