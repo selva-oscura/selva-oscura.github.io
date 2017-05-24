07c9c2c301d5d453db34d3805f8a72f96972e024
Carol St.Louis <1.selva.oscura@gmail.com>
2017-05-23 20:47:16 -0700

    removed redundant div; removed hard-coded copyright (moved to Copyright)

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import FindMe from './FindMe';
+import Copyright from './Copyright';
 
 const Contact = () => {
 	return (
@@ -9,51 +10,43 @@ const Contact = () => {
 					<h2>Have me make stuff for you.</h2>
 					<p>Ornare nulla proin odio consequat sapien vestibulum ipsum sed lorem.</p>
 				</header>
-				<div>
-					<div className="row">
-						<div className="12u">
-							<form method="post" action="#">
-								<div>
-									<div className="row">
-										<div className="6u 12u(mobile)">
-											<input type="text" name="name" id="name" placeholder="Name" />
-										</div>
-										<div className="6u 12u(mobile)">
-											<input type="text" name="email" id="email" placeholder="Email" />
-										</div>
+				<div className="row">
+					<div className="12u">
+						<form method="post" action="#">
+							<div>
+								<div className="row">
+									<div className="6u 12u(mobile)">
+										<input type="text" name="name" id="name" placeholder="Name" />
 									</div>
-									<div className="row">
-										<div className="12u">
-											<input type="text" name="subject" id="subject" placeholder="Subject" />
-										</div>
-									</div>
-									<div className="row">
-										<div className="12u">
-											<textarea name="message" id="message" placeholder="Message"></textarea>
-										</div>
-									</div>
-									<div className="row 200%">
-										<div className="12u">
-											<ul className="actions">
-												<li><input type="submit" value="Send Message" /></li>
-												<li><input type="reset" value="Clear Form" className="alt" /></li>
-											</ul>
-										</div>
+									<div className="6u 12u(mobile)">
+										<input type="text" name="email" id="email" placeholder="Email" />
 									</div>
 								</div>
-							</form>
-						</div>
+								<div className="row">
+									<div className="12u">
+										<input type="text" name="subject" id="subject" placeholder="Subject" />
+									</div>
+								</div>
+								<div className="row">
+									<div className="12u">
+										<textarea name="message" id="message" placeholder="Message"></textarea>
+									</div>
+								</div>
+								<div className="row 200%">
+									<div className="12u">
+										<ul className="actions">
+											<li><input type="submit" value="Send Message" /></li>
+											<li><input type="reset" value="Clear Form" className="alt" /></li>
+										</ul>
+									</div>
+								</div>
+							</div>
+						</form>
 					</div>
-
-					<FindMe />
-					
 				</div>
-				<footer>
-					<ul id="copyright">
-						<li>&copy; Untitled. All rights reserved.</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
-					</ul>
-				</footer>
+				<FindMe />
 			</article>
+			<Copyright />
 		</div>
 	)
 }
