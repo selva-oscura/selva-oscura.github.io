dada0d2174be7aae91171d319d27d4db79bc24b1
Carol St.Louis <1.selva.oscura@gmail.com>
2017-05-23 22:38:06 -0700

    linked button to Profile section

--- src/components/Home.js
+++ src/components/Home.js
@@ -13,7 +13,7 @@ const Home = () => {
 							<h1>Hi. I'm <strong>Carol &nbsp;St. Louis</strong>.</h1>
 						</header>
 						<p>And this is <strong>Miniport</strong>, a free, fully responsive HTML5 site template designed by <a href="http://twitter.com/ajlkn">AJ</a> for <a href="http://html5up.net">HTML5 UP</a> &amp; released under the <a href="http://html5up.net/license">CCA license</a>.</p>
-						<a href="#work" className="button big scrolly">Learn about what I do</a>
+						<a href="#profile" className="button big scrolly">Learn about what I do</a>
 					</div>
 				</div>
 			</article>
