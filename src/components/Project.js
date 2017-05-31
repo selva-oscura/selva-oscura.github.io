54399d9e80aa389d90bafe6bda32fc4ddd811177
Carol St.Louis <1.selva.oscura@gmail.com>
2017-05-30 22:37:32 -0700

    created onClick event handler to pass target to props.selectProject

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,13 +1,18 @@
 import React from 'react';
 
 const Project = (props) => {
-	console.log(props)
+	const handleClick = () => {
+		props.selectProject(props.project.target)
+	}
 	return (
-		<div 
-			className="4u 12u(mobile)"
-		>
-			<article className="box style2">
-				<a href="blah" className="image featured"><img src={`./public/images/pic0${props.num%4+1}.jpg`} alt={`screenshot of ${props.project.name}`} /></a>
+		<div className="4u 12u(mobile)">
+			<article
+				className="box style2"
+				onClick={handleClick}
+			>
+				<div className="image featured">
+					<img src={`./public/images/pic0${props.num%4+1}.jpg`} alt={`screenshot of ${props.project.name}`} />
+				</div>
 				<h3>{props.project.name}</h3>
 			</article>
 		</div>
