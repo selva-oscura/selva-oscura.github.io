13985640105051d10ac273581c4643bf0908b11f
Carol St.Louis <1.selva.oscura@gmail.com>
2017-05-31 13:48:00 -0700

    created filters array and imported/displayed ProjectFilterButton components based upon those filters

---
+++ src/components/ProjectFilters.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ProjectFilterButton from './ProjectFilterButton';
+
+const ProjectFilters = (props) => {
+	const filters = [
+		{'all': 'All'},
+		{'meteor': 'Meteor.js'},
+		{'node': 'Node.js'},
+		{'php': 'PHP'},
+		{'lamp': 'PHP / MySQL'},
+		{'pwa': 'Progressive Web App (PWA)'},
+		{'react': 'React.js'},
+		{'webrtc': 'WebRTC'},
+	]
+	return (
+		<div className='project-filters'>
+			{ filters.map((filter, i)=>(
+				<ProjectFilterButton 
+					key={i} 
+					filter={filter}
+				/>
+				)) 
+			}
+		</div>
+	)
+}
+
+export default ProjectFilters;
