06da646397ffed253c1c6d3c2d9d46209791b7a4
Carol St.Louis <1.selva.oscura@gmail.com>
2017-05-31 19:59:10 -0700

    added 'alt' className when button attribute was in projectFilters (state)

--- src/components/ProjectFilterButton.js
+++ src/components/ProjectFilterButton.js
@@ -1,12 +1,18 @@
 import React from 'react';
 
 const ProjectFilterButton = (props) => {
+	let className="";
+	if(props.projectFilters.includes(Object.keys(props.filter)[0]) ||
+		(props.projectFilters.length===0 && Object.keys(props.filter)[0]==="all")){
+		className = "alt";
+	}
 	const handleClick = () => {
 		props.setProjectFilters(Object.keys(props.filter));
 	}
 	return (
 		<button
 			onClick={handleClick}
+			className={className}
 		>
 			{Object.values(props.filter)}
 		</button>
