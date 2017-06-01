99c9dce07d132b610412fce9c40a72078e1ea253
Carol St.Louis <1.selva.oscura@gmail.com>
2017-05-31 19:58:11 -0700

    passed projectFilters (state) to ProjectFilterButton component

--- src/components/ProjectFilters.js
+++ src/components/ProjectFilters.js
@@ -19,6 +19,7 @@ const ProjectFilters = (props) => {
 					key={i} 
 					filter={filter} 
 					setProjectFilters={props.setProjectFilters}
+					projectFilters={props.projectFilters}
 				/>
 				)) 
 			}
