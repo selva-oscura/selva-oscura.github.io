28891f3b8bdb637c9b267943f0d78768a31ee70a
Carol St.Louis <1.selva.oscura@gmail.com>
2017-06-08 23:24:18 -0700

    enclosed links to project website/repo/codepen in paragraphs

--- src/components/ProjectModal.js
+++ src/components/ProjectModal.js
@@ -31,11 +31,11 @@ const ProjectModal = (props) => {
 				</div>
 				<div className="modal-footer">
 					<div className="half text-left">
-						{ props.project.web ? <a href={props.project.web} target={props.project.target}><span className="icon featured fa-globe"></span> Project Website</a> : " " }
-						{ props.project.codepen ? <a href={props.project.codepen} target={props.project.target}><span className="icon featured fa-codepen"></span> Project Codepen</a> : " " }
+						{ props.project.web ? <p><a href={props.project.web} target={props.project.target}><span className="icon featured fa-globe"></span> Project Website</a></p> : " " }
+						{ props.project.codepen ? <p><a href={props.project.codepen} target={props.project.target}><span className="icon featured fa-codepen"></span> Project Codepen</a></p> : " " }
 					</div>
 					<div className="half text-right">
-						{ props.project.git ? <a href={props.project.git} target={props.project.target}><span className="icon featured fa-github"></span> Project Repo</a> : " " }
+						{ props.project.git ? <p><a href={props.project.git} target={props.project.target}><span className="icon featured fa-github"></span> Project Repo</a></p> : " " }
 					</div>
 				</div>
 			</div>
