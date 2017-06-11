30106ee3cc16ce6f340b58b0a2110010ae526629
Carol St.Louis <1.selva.oscura@gmail.com>
2017-06-11 16:12:32 -0700

    formatted FindMe to be standalone article in App rather than part of Contact; changed to style4

--- src/components/FindMe.js
+++ src/components/FindMe.js
@@ -1,22 +1,25 @@
 import React from 'react';
+import Copyright from './Copyright';
 
 const FindMe = () => {
 	return (
-		<div className="row">
-			<div className="12u">
-				<hr />
-				<h3>Find me on ...</h3>
-				<ul className="social">
-					<li><a href="blah" className="icon fa-twitter"><span className="label">Twitter</span></a></li>
-					<li><a href="blah" className="icon fa-facebook"><span className="label">Facebook</span></a></li>
-					<li><a href="blah" className="icon fa-dribbble"><span className="label">Dribbble</span></a></li>
-					<li><a href="blah" className="icon fa-linkedin"><span className="label">LinkedIn</span></a></li>
-					<li><a href="blah" className="icon fa-tumblr"><span className="label">Tumblr</span></a></li>
-					<li><a href="blah" className="icon fa-google-plus"><span className="label">Google+</span></a></li>
-					<li><a href="blah" className="icon fa-github"><span className="label">Github</span></a></li>
-				</ul>
-				<hr />
-			</div>
+		<div className="wrapper style4">
+			<article className="container">
+				<div className="row">
+					<div className="12u">
+						<h3>Find me on ...</h3>
+						<ul className="social">
+							<li><a href="blah" className="icon fa-twitter"><span className="label">Twitter</span></a></li>
+							<li><a href="blah" className="icon fa-facebook"><span className="label">Facebook</span></a></li>
+							<li><a href="blah" className="icon fa-linkedin"><span className="label">LinkedIn</span></a></li>
+							<li><a href="blah" className="icon fa-tumblr"><span className="label">Tumblr</span></a></li>
+							<li><a href="blah" className="icon fa-google-plus"><span className="label">Google+</span></a></li>
+							<li><a href="blah" className="icon fa-github"><span className="label">Github</span></a></li>
+						</ul>
+					</div>
+				</div>
+			</article>
+			<Copyright />
 		</div>
 	)
 }
