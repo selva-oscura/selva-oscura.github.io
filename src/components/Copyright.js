213291b5f4c1daa0c638ad01178e6c4f830e7900
Carol St.Louis <1.selva.oscura@gmail.com>
2017-06-11 16:46:06 -0700

    fleshed out copyright and credits for design & images;

--- src/components/Copyright.js
+++ src/components/Copyright.js
@@ -4,7 +4,9 @@ const Copyright = () => {
 	return (
 		<footer>
 			<ul id="copyright">
-				<li>&copy; Untitled. All rights reserved.</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
+				<li>&copy; Carol  St. Louis, 2017. All rights reserved.</li>
+				<li>Design: modified from <a href="http://html5up.net">HTML5 UP</a>&rsquo;s theme <a href="https://html5up.net/miniport">Miniport</a>.</li>
+				<li>Images: <a href="https://unsplash.com/">Unsplash</a>.</li>
 			</ul>
 		</footer>
 	)
