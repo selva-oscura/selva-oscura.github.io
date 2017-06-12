a1ee157beefe0d3490451ead725a438fc615d14e
Carol St.Louis <1.selva.oscura@gmail.com>
2017-06-11 17:43:12 -0700

    pruned social media & added the links

--- src/components/FindMe.js
+++ src/components/FindMe.js
@@ -9,12 +9,10 @@ const FindMe = () => {
 					<div className="12u">
 						<h3>Find me on ...</h3>
 						<ul className="social">
-							<li><a href="blah" className="icon fa-twitter"><span className="label">Twitter</span></a></li>
-							<li><a href="blah" className="icon fa-facebook"><span className="label">Facebook</span></a></li>
-							<li><a href="blah" className="icon fa-linkedin"><span className="label">LinkedIn</span></a></li>
-							<li><a href="blah" className="icon fa-tumblr"><span className="label">Tumblr</span></a></li>
-							<li><a href="blah" className="icon fa-google-plus"><span className="label">Google+</span></a></li>
-							<li><a href="blah" className="icon fa-github"><span className="label">Github</span></a></li>
+							<li><a href="https://www.linkedin.com/in/stlouisc/" className="icon fa-linkedin"><span className="label">LinkedIn</span></a></li>
+							<li><a href="https://github.com/selva-oscura" className="icon fa-github"><span className="label">Github</span></a></li>
+							<li><a href="https://codepen.io/stlouis_c/" className="icon fa-codepen"><span className="label">Codepen</span></a></li>
+							<li><a href="https://twitter.com/1_selva_oscura" className="icon fa-twitter"><span className="label">Twitter</span></a></li>
 						</ul>
 					</div>
 				</div>
