0a7fa486d158d729b93bf50ec85c26c644678d11
Carol St.Louis <1.selva.oscura@gmail.com>
2017-07-01 00:00:36 -0700

    specified *background* images from unsplash

--- src/components/Copyright.js
+++ src/components/Copyright.js
@@ -6,7 +6,7 @@ const Copyright = () => {
 			<ul id="copyright">
 				<li>&copy; Carol  St. Louis, 2017. All rights reserved.</li>
 				<li>Design: modified from <a href="http://html5up.net">HTML5 UP</a>&rsquo;s theme <a href="https://html5up.net/miniport">Miniport</a>.</li>
-				<li>Images: <a href="https://unsplash.com/">Unsplash</a>.</li>
+				<li>Background Images: <a href="https://unsplash.com/">Unsplash</a>.</li>
 			</ul>
 		</footer>
 	)
