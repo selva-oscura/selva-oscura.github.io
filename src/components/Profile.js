30b4b3fe18d186413dc43e22a6a361abc89a7559
Carol St.Louis <1.selva.oscura@gmail.com>
2017-07-01 00:17:53 -0700

    fleshed-out Skills/Experience re: JS, frameworks, tools (needs trimming, reformatting)

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -49,9 +49,16 @@ const Profile = () => {
 					<header>
 						<h2>Skills &amp; Experience</h2>
 						<ul>
-							<li>My more recent client projects are Node.js full-stack, usually using Meteor.js or MERN (MongoDB, Express.js, React, and Node.js).</li>
-							<li>Older client projects utilized the LAMP stack (Linux, Apache server, MySQL, and PHP).</li>
-							<li>Personal and hackathon projects have focused on JavaScript technologies and include: Progressive Web App (PWA) games utilizing React, web workers, and local storage; Meteor-based hackathon apps that used WebRTC; a Meteor-based hackathon app  that used geolocation and search criteria to match would-be pet owners with adoptable animals.</li>
+							<li>I'm a full-stack web developer with 4 years of experience.  I specialize in developing websites which are fast, easy-to-use, and beautiful.</li>
+							<li>JavaScript / ECMAScript is my favorite language for web development, courtesy of its flexibility and the incredible development of powerful libraries and frameworks in recent years.  My most recent projects are Node.js full-stack, using Meteor.js, the MERN stack (MongoDB, Express.js, React, and Node.js) or the MEAN stack (MongoDB, Express.js, Angular, and Node.js).</li>
+							<li>For client projects, I have automated server-side cron jobs on Node.js that allow automation of notifications and clearing of holds for uncompleted transactions (see Door-to-Door Shines below or <a href="https://www.doortodoorshines.com" target="shines">live</a>), and updating of data that clients are monitoring (see Zyrl below)</li>
+							<li>In my personal projects and at hackathons, I've enjoyed exploring HTML5's new APIs, new JS libraries, and the ever greater browser capabilities that make it possible:</li>
+								<ul>
+									<li>to use React, web workers, and local storage on the client to make Progressive Web App games (see Sudoku, Hangman, and Tic-Tac-Toe below),</li>
+									<li>use Meteor.js, MongoDB, Materialize.css (an adaptation of Google's Material Design), and PeerJS to implement a Portal-themed WebRTC live video chat site (PoRTCal),</li>
+									<li>use Meteor.js, TwitterBootstrap, MongoDB, and geolocation & search criteria to match would-be pet owners with adoptable animals (see Animal Rescue below).</li>
+								</ul>
+							<li>Older clients projects and bootcamp projects utilized the LAMP stack (Linux, Apache server, MySQL, and PHP) and Ruby on Rails.</li>
 							<li>I&rsquo;ve also dabbled in Elm, GoLang, Python, and Ruby.</li>
 						</ul>
 					</header>
