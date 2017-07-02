21afc68928bf10510ae3be8c0d6c156786ef8223
Carol St.Louis <1.selva.oscura@gmail.com>
2017-07-02 00:22:12 -0700

    prettified layout of call to LinkToProfile; corrected passing of selectProject;

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -52,7 +52,13 @@ const Profile = (props) => {
 						<div id="summary">
 							<p>I'm a full-stack web developer with 4 years of experience.  I specialize in developing websites which are fast, easy-to-use, and beautiful.</p>
 							<p><a href="https://en.wikipedia.org/wiki/JavaScript" target="javascript">JavaScript</a> / <a href="https://en.wikipedia.org/wiki/ECMAScript" target="ecmascript">ECMAScript</a> is my favorite language for web development because of its flexibility and the incredible pace of development of powerful libraries and frameworks in recent years.  My most recent projects are Node.js full-stack, using <a href="https://www.meteor.com/" target="meteor">Meteor.js</a>, the MERN stack (<a href="https://www.mongodb.com/" target="mongodb">MongoDB</a>, <a href="https://expressjs.com/" target="express">Express.js</a>, <a href="https://facebook.github.io/react/" target="react">React</a>and <a href="https://nodejs.org/en/" target="node">Node.js</a>), or the MEAN stack (<a href="https://www.mongodb.com/" target="mongodb">MongoDB</a>, <a href="https://expressjs.com/" target="express">Express.js</a>, <a href="https://angular.io/" target="angular">Angular</a>, and <a href="https://nodejs.org/en/" target="node">Node.js</a>).</p>
-							<p>For two recent client projects, I used Meteor's reactive programming framework to develop fast, secure websites, backed by MongoDB's databases, and automated Node.js server-side cron jobs that automated notifications and clearing of holds for uncompleted transactions (see Door-to-Door Shines below or <a href="https://www.doortodoorshines.com" target="shines">live</a>), and updating of data that clients are monitoring (see Zyrl <LinkToProject text="below" target="zyrl" selectProject="selectProject" />)</p>
+							<p>For two recent client projects, I used Meteor's reactive programming framework to develop fast, secure websites, backed by MongoDB's databases, and automated Node.js server-side cron jobs that automated notifications and clearing of holds for uncompleted transactions (see Door-to-Door Shines below or <a href="https://www.doortodoorshines.com" target="shines">live</a>), and updating of data that clients are monitoring (see Zyrl
+									<LinkToProject
+										text="below"
+										target="zyrl"
+										selectProject={props.selectProject}
+									/>
+								)</p>
 							<p>In my personal projects and at hackathons, I've enjoyed exploring HTML5's new APIs, the myriad JS libraries & frameworks, and the ever greater browser capabilities:</p>
 									<ul>
 										<li>to use React, web workers, and local storage on the client to make <a href="https://developers.google.com/web/progressive-web-apps/" target="pwa">Progressive Web App</a> games (see Sudoku, Hangman, and Tic-Tac-Toe below),</li>
