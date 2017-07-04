0ae405320e46c2d50d54f5b15630645d96a7d575
Carol St.Louis <1.selva.oscura@gmail.com>
2017-07-03 22:58:13 -0700

    passed project data from project description to LinkToProject

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -51,19 +51,56 @@ const Profile = (props) => {
 						<h2>Skills &amp; Experience</h2>
 						<div id="summary">
 							<p>I'm a full-stack web developer with 4 years of experience.  I specialize in developing websites which are fast, easy-to-use, and beautiful.</p>
-							<p><a href="https://en.wikipedia.org/wiki/JavaScript" target="javascript">JavaScript</a> / <a href="https://en.wikipedia.org/wiki/ECMAScript" target="ecmascript">ECMAScript</a> is my favorite language for web development because of its flexibility and the incredible pace of development of powerful libraries and frameworks in recent years.  My most recent projects are Node.js full-stack, using <a href="https://www.meteor.com/" target="meteor">Meteor.js</a>, the MERN stack (<a href="https://www.mongodb.com/" target="mongodb">MongoDB</a>, <a href="https://expressjs.com/" target="express">Express.js</a>, <a href="https://facebook.github.io/react/" target="react">React</a>and <a href="https://nodejs.org/en/" target="node">Node.js</a>), or the MEAN stack (<a href="https://www.mongodb.com/" target="mongodb">MongoDB</a>, <a href="https://expressjs.com/" target="express">Express.js</a>, <a href="https://angular.io/" target="angular">Angular</a>, and <a href="https://nodejs.org/en/" target="node">Node.js</a>).</p>
-							<p>For two recent client projects, I used Meteor's reactive programming framework to develop fast, secure websites, backed by MongoDB's databases, and automated Node.js server-side cron jobs that automated notifications and clearing of holds for uncompleted transactions (see Door-to-Door Shines below or <a href="https://www.doortodoorshines.com" target="shines">live</a>), and updating of data that clients are monitoring (see Zyrl
+							<p><a href="https://en.wikipedia.org/wiki/JavaScript" target="javascript">JavaScript</a> / <a href="https://en.wikipedia.org/wiki/ECMAScript" target="ecmascript">ECMAScript</a> is my favorite language for web development because of its flexibility and the incredible pace of development of powerful libraries and frameworks in recent years.  My most recent projects are Node.js full-stack, using <a href="https://www.meteor.com/" target="meteor">Meteor.js</a>, the MERN stack (<a href="https://www.mongodb.com/" target="mongodb">MongoDB</a>, <a href="https://expressjs.com/" target="express">Express.js</a>, <a href="https://facebook.github.io/react/" target="react">React</a>, and <a href="https://nodejs.org/en/" target="node">Node.js</a>), or the MEAN stack (<a href="https://www.mongodb.com/" target="mongodb">MongoDB</a>, <a href="https://expressjs.com/" target="express">Express.js</a>, <a href="https://angular.io/" target="angular">Angular</a>, and <a href="https://nodejs.org/en/" target="node">Node.js</a>).</p>
+							<p>For two recent client projects, I used Meteor's reactive programming framework to develop fast, secure websites, backed by MongoDB's databases, and utilized Node.js server-side cron jobs to automate notifications &nbsp; reports and to clear holds for uncompleted transactions (see Door-to-Door Shines
 									<LinkToProject
-										text="below"
+										text="description"
+										target="shines"
+										selectProject={props.selectProject}
+									/>
+									&nbsp;or <a href="https://www.doortodoorshines.com" target="shines">live</a>), and to update data that clients are monitoring (see
+									<LinkToProject
+										text="Zyrl"
 										target="zyrl"
 										selectProject={props.selectProject}
 									/>
 								)</p>
 							<p>In my personal projects and at hackathons, I've enjoyed exploring HTML5's new APIs, the myriad JS libraries & frameworks, and the ever greater browser capabilities:</p>
 									<ul>
-										<li>to use React, web workers, and local storage on the client to make <a href="https://developers.google.com/web/progressive-web-apps/" target="pwa">Progressive Web App</a> games (see Sudoku, Hangman, and Tic-Tac-Toe below),</li>
-										<li>to use Meteor.js, MongoDB, <a href="http://materializecss.com/" target="materialize">Materialize.css</a> (an adaptation of Google's Material Design), and <a href="http://peerjs.com/" target="peerjs">PeerJS</a> to implement a Portal-themed <a href="https://webrtc.org/" target="webrtc">WebRTC</a> live video chat site (see PoRTCal below),</li>
-										<li>to use Meteor.js, <a href="https://v4-alpha.getbootstrap.com/" target="bootstrap">TwitterBootstrap</a>, MongoDB, and geolocation & search criteria to match would-be pet owners with adoptable animals (see Animal Rescue below).</li>
+										<li>to use React, web workers, and local storage on the client to make <a href="https://developers.google.com/web/progressive-web-apps/" target="pwa">Progressive Web App</a> games (see
+											<LinkToProject
+												text="Sudoku"
+												target="sudoku"
+												selectProject={props.selectProject}
+											/>
+											,
+											<LinkToProject
+												text="Hangman"
+												target="hangman"
+												selectProject={props.selectProject}
+											/>
+											, and
+											<LinkToProject
+												text="Tic-Tac-Toe"
+												target="tic-tac-toe"
+												selectProject={props.selectProject}
+											/>
+											),</li>
+										<li>to use Meteor.js, MongoDB, <a href="http://materializecss.com/" target="materialize">Materialize.css</a> (an adaptation of Google's Material Design), and <a href="http://peerjs.com/" target="peerjs">PeerJS</a> to implement a Portal-themed <a href="https://webrtc.org/" target="webrtc">WebRTC</a> live video chat site (see
+												<LinkToProject
+													text="PoRTCal"
+													target="portcal"
+													selectProject={props.selectProject}
+												/>
+											),
+										</li>
+										<li>to use Meteor.js, <a href="https://v4-alpha.getbootstrap.com/" target="bootstrap">TwitterBootstrap</a>, MongoDB, and geolocation & search criteria to match would-be pet owners with adoptable animals (see
+												<LinkToProject
+													text="Animal Rescue"
+													target="animal_rescue"
+													selectProject={props.selectProject}
+												/>
+										).</li>
 									</ul>
 							<p>Older clients projects and bootcamp projects utilized the LAMP stack (Linux, Apache server, MySQL, and PHP) and Ruby on Rails.</p>
 							<p>I&rsquo;ve also dabbled in Elm, GoLang, Python, and Ruby.</p>
