12aad65854d0482664a0faf758fac273fea5e4ee
Carol St.Louis <1.selva.oscura@gmail.com>
2017-07-06 01:51:15 -0700

    started adding properties and event handlers; removed footer;

--- src/components/ScreenShotsModal.js
+++ src/components/ScreenShotsModal.js
@@ -4,27 +4,27 @@ const ScreenShotsModal = (props) => {
 
 	const handleClick = (e) => {
 		if(e.target.className==="modal"){
-			props.unselectProject()
+			props.unselectProjectScreenshots()
 		}
 	}
 
+			// style={ props.selectedProjectScreenshots===props.project.target ? {'display':'block'} : {'display':'none'}}
 	let modalContent = "modal-content";
-	if(props.selectedProject===props.project.target && props.deselectedProject){
+	if(props.selectedProjectScreenshots===props.project.target && props.deselectedProjectScreenshots){
 		modalContent = "modal-out";
 	}
 
 	return (
 		<div
 			className="modal"
-			id={props.project.target}
-			style={ props.selectedProject===props.project.target ? {'display':'block'} : {'display':'none'}}
+			id={`screenshots${props.project.target}`}
 			onClick={handleClick}
 		>
 			<div className={modalContent}>
 				<div className="modal-header">
 					<span 
 						className="modal-close"
-						onClick={props.unselectProject}
+						onClick={props.unselectProjectScreenshots}
 					>
 						x
 					</span>
@@ -32,22 +32,16 @@ const ScreenShotsModal = (props) => {
 				</div>
 				<div className="screenshots-body flex-grid-uneven-thirds">
 					<div className="col screenshot-nav previous">
-
+						Previous
 					</div>
-					<div className="col">
-
+					<div className="col main">
+						<img 
+							className="screenshot"
+							src={`./public/images/pic00.jpg`}
+						/>
 					</div>
-				</div>
 					<div className="col screenshot-nav next">
-
-					</div>
-				<div className="modal-footer">
-					<div className="half text-left">
-						{ props.project.web ? <p><a href={props.project.web} target={props.project.target}><span className="icon featured fa-globe"></span> Project Website</a></p> : " " }
-						{ props.project.codepen ? <p><a href={props.project.codepen} target={props.project.target}><span className="icon featured fa-codepen"></span> Project Codepen</a></p> : " " }
-					</div>
-					<div className="half text-right">
-						{ props.project.git ? <p><a href={props.project.git} target={props.project.target}><span className="icon featured fa-github"></span> Project Repo</a></p> : " " }
+						Next
 					</div>
 				</div>
 			</div>
