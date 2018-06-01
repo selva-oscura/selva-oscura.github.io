df982423f828f17a4b81e376eba0d02d3e79e3c2
Carol Diane  St. Louis <stlouis_c@yahoo.com>
2018-05-31 22:35:59 -0700

    Copyright: updated copyright year, removed extraneous punctuation

--- src/components/Copyright.js
+++ src/components/Copyright.js
@@ -4,16 +4,16 @@ const Copyright = () => {
   return (
     <footer>
       <ul id="copyright">
-        <li>&copy; Carol St. Louis, 2017. All rights reserved.</li>
+        <li>&copy; Carol St. Louis, 2018. All rights reserved.</li>
         <li>
           Design: modified from <a href="http://html5up.net">
             HTML5 UP
           </a>&rsquo;s theme <a href="https://html5up.net/miniport">
             Miniport
-          </a>.
+          </a>
         </li>
         <li>
-          Background Images: <a href="https://unsplash.com/">Unsplash</a>.
+          Background Images: <a href="https://unsplash.com/">Unsplash</a>
         </li>
       </ul>
     </footer>
