451aabc787439d05e55fbc45b54a557be910d74b
Carol Diane  St. Louis <stlouis_c@yahoo.com>
2018-05-31 20:45:23 -0700

    ProjectFilters: added GraphQL to filters

--- src/components/ProjectFilters.js
+++ src/components/ProjectFilters.js
@@ -9,6 +9,7 @@ const ProjectFilters = props => {
     { meteor: 'Meteor' },
     { react: 'React' },
     { redux: 'Redux' },
+    { graphQL: 'GraphQL'},
     { angular: 'Angular' },
     { pwa: 'Progressive Web App (PWA)' },
     { webrtc: 'WebRTC' },
